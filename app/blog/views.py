1f6df711aa14745beedd848d20262a42734f0315
Legend_7ok <dimtkac777@gmail.com>
2026-02-11 15:06:00 +0200

    Add tagging and similar posts recommendations feature

--- app/blog/views.py
+++ app/blog/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
+from django.db.models import Count
 
 
 class PostListView(ListView):
@@ -13,8 +15,12 @@ class PostListView(ListView):
     paginate_by = 3
     template_name = 'blog/post/list.html'
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -29,7 +35,7 @@ def post_list(request):
 
     return render(request,
                   'blog/post/list.html',
-                  {'posts': posts})
+                  {'posts': posts, 'tag': tag})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
@@ -40,11 +46,20 @@ def post_detail(request, year, month, day, post):
                              publish__day=day)
     # Список активных комментариев к этому посту
     comments = post.comments.filter(active=True)
+
     # Форма для комментирования пользователями
     form = CommentForm()
 
+    # Список схожих постов
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
     return render(request, 'blog/post/detail.html',
-                  {'post': post, 'comments': comments, 'form': form})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form,
+                   'similar_posts': similar_posts})
 
 def post_share(request, post_id):
     # Извлечь пост по идентификатору id
